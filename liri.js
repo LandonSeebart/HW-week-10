6a29e360bef89f88e4126d4196e92415858a2dd7
landon <landon.seebart@gmail.com>
2018-04-15 12:58:18 -0500

    added spotify keys

--- liri.js
+++ liri.js
@@ -1,34 +1,61 @@
 require("dotenv").config();
 
+//======Variables=========================
 // var spotify = new Spotify(keys.spotify);
 // var client = new Twitter(keys.twitter);
 
-// `my-tweets`
+//Install necessary Node Pakcages
 var Twitter = require('twitter');
-
-// `spotify-this-song`
 var Spotify = require('node-spotify-api');
-
-// `movie-this`
 var request = require('request');
 
-// Grab or assemble the movie name and store it in a variable called "movieName"
-var movieName = process.argv[2];
+//Define user inputs
+var action = process.argv[2];
+var input = process.argv[3];
 
-// Then run a request to the OMDB API with the movie specified
-var queryUrl = "http://www.omdbapi.com/?t=" + movieName + "&y=&plot=short&apikey=trilogy";
-console.log(queryUrl);
-
-// Then create a request to the queryUrl
-request(queryUrl, function(error, response, body){
+//=============Main================
+switch (action) {
+    // case "my-tweets":
+    //   myTweets();
+    //   break;
     
-    // If the request is successful
-    if (!error && response.statusCode === 200) {
+    // case "spotify-this-song":
+    //   spotifyThis();
+    //   break;
+    
+    // case "withdraw":
+    //   withdraw();
+    //   break;
+    
+    case "movie-this":
+      movieThis(input);
+      break;
+    }
 
-        // Then log the Release Year for the movie
-        console.log("Release Year: " + JSON.parse(body).Year);
-      }   
-})
+//=======================Functions==========================
 
+function movieThis(movieName) {
+
+    var queryUrl = "http://www.omdbapi.com/?t=" + movieName + "&y=&plot=short&apikey=trilogy";
+
+    //Create a request to the queryUrl
+    request(queryUrl, function(error, response, body){
+        
+        // If the request is successful
+        if (!error && response.statusCode === 200) {
+
+            // Then log the following for the movie: Title, Release year, IMDB Rating,
+            // Rotten Tomatoes Rating, Country, Language, Plot, and Actors. 
+            console.log(`Title: ${JSON.parse(body).Title}`);
+            console.log(`Release Year: ${JSON.parse(body).Year}`);
+            console.log(`IMDB Rating: ${JSON.parse(body).imdbRating}`);
+            console.log(`Rotten Tomatoes Rating: ${JSON.parse(body).Ratings[2].Value}`);
+            console.log(`Country: ${JSON.parse(body).Country}`);
+            console.log(`Language: ${JSON.parse(body).Language}`);
+            console.log(`Plot: ${JSON.parse(body).Plot}`);
+            console.log(`Actors: ${JSON.parse(body).Actors}`);
+        }
+    })
+};
 
 // `do-what-it-says`
